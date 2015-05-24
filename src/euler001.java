1793e0cd6dda274c0dd6093296db8bf4862f4983
Manan <mananshah9328@gmail.com>
2015-05-24 20:06:02 +0530

    euler001

--- src/euler001.java
+++ src/euler001.java
@@ -4,32 +4,24 @@ import java.util.Scanner;
  * Created by Manan on 24-05-2015.
  */
 public class euler001 {
-
-    private static int MAX_LIMIT=10000001;
-    public static int MAX_ARRAY_VALUE=MAX_LIMIT/3 + MAX_LIMIT/5;
     public static void main(String args[]){
         Scanner scanner = new Scanner(System.in);
-        int test = scanner.nextInt();
-        int arr[] = new int[MAX_ARRAY_VALUE];
-        arr[0]=0;
-        for (int i=1;i<MAX_ARRAY_VALUE;i++){
-            arr[i]=arr[i-1]+lesser((int)Math.ceil(((arr[i-1]+1)/3.0))*3,(int)Math.ceil(((arr[i-1]+1)/5.0))*5);
-        }
-
+        long test = scanner.nextLong();
 
         while (test>0){
-            int temp = scanner.nextInt();
-            for (int i=1;i<101;i++)
-            {
-                System.out.println(arr[i]);
-            }
-           test--;
+            long number = scanner.nextLong();
+            long result = function(number);
+            System.out.println(result);
+            test--;
         }
     }
 
-    private static int lesser(int a, int b) {
-        if(a<=b) return 3;
-        else return 5;
+    private static long function(long number) {
+        long x_3=(int) Math.ceil(number/3.0)-1;
+        long x_5=(int) Math.ceil(number/5.0)-1;
+        long x_15=(int) Math.ceil(number/15.0)-1;
 
+        long answer = 3*(x_3 * (x_3+1))/2 + 5*(x_5 * (x_5+1))/2 - 15*(x_15 * (x_15+1))/2;
+        return answer;
     }
 }
